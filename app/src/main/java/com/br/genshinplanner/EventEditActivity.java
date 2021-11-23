89201835b423390b0e8632f1f55fa2bf06b5e58e
Rodrigo Flores <rodrigo.flores@l2code.com.br>
2021-11-23 19:32:38 -0300

    projeto inicial

---
+++ app/src/main/java/com/br/genshinplanner/EventEditActivity.java
@@ -0,0 +1,47 @@
+package com.br.genshinplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.time.LocalTime;
+
+public class EventEditActivity extends AppCompatActivity
+{
+    private EditText eventNameET;
+    private EditText resinToSpendET;
+    private TextView eventDateTV;
+    private TextView eventTimeTV;
+
+    private LocalTime time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit);
+        initWidgets();
+        time = LocalTime.now();
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+    }
+
+    private void initWidgets()
+    {
+        eventNameET = findViewById(R.id.eventNameET);
+        eventDateTV = findViewById(R.id.eventDateTV);
+        eventTimeTV = findViewById(R.id.eventTimeTV);
+    }
+
+    public void saveEventAction(View view)
+    {
+        String eventName = eventNameET.getText().toString();
+        String resinToSpend = resinToSpendET.getText().toString();
+        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time, resinToSpend);
+        Event.eventsList.add(newEvent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/br/genshinplanner/MainActivity.java
@@ -0,0 +1,88 @@
+package com.br.genshinplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import static com.br.genshinplanner.CalendarUtils.daysInMonthArray;
+import static com.br.genshinplanner.CalendarUtils.monthYearFromDate;
+
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
+{
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initWidgets();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setMonthView();
+    }
+
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    public void previousMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
+        if(date != null)
+        {
+            CalendarUtils.selectedDate = date;
+            setMonthView();
+        }
+    }
+
+    public void weeklyAction(View view)
+    {
+        startActivity(new Intent(this, WeekViewActivity.class));
+    }
+}
+
+
+
+
+
+
+
+
