26ab425b5698dad17fbfff507b7af7bdcc977bb3
Rodrigo Flores <rodrigo.flores@l2code.com.br>
2021-12-02 18:51:43 -0300

    adicionando delete de evento

--- app/src/main/java/com/br/genshinplanner/EventAdapter.java
+++ app/src/main/java/com/br/genshinplanner/EventAdapter.java
@@ -1,25 +1,36 @@
 package com.br.genshinplanner;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.br.genshinplanner.sqlite.DAO;
 
 import java.util.List;
 import java.util.Optional;
 
 public class EventAdapter extends ArrayAdapter<EventEntity>
 {
+    private Context eventContext;
+    private EventAdapter adapter;
+
     public EventAdapter(@NonNull Context context, List<EventEntity> events)
     {
         super(context, 0, events);
+        eventContext = context;
+        adapter = this;
     }
 
     @NonNull
@@ -33,6 +44,7 @@ public class EventAdapter extends ArrayAdapter<EventEntity>
 
         TextView eventCellTV = convertView.findViewById(R.id.eventCellTV);
         ImageView eventCellIV = convertView.findViewById(R.id.eventCellIV);
+        Button delete = convertView.findViewById(R.id.btnDelete);
 
         Optional<Domain> domain = Domain.getDomains().stream().filter(d -> d.getDomainName().equals(event.getDomain())).findFirst();
         if(domain.isPresent()) {
@@ -40,6 +52,17 @@ public class EventAdapter extends ArrayAdapter<EventEntity>
             eventCellTV.setText(eventTitle);
             eventCellIV.setImageResource(domain.get().getDomainImage());
         }
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    DAO dao = new DAO(eventContext);
+                    dao.deleteOne(event.getId());
+                    adapter.remove(event);
+                    adapter.notifyDataSetChanged();
+            }
+        });
         return convertView;
     }
+
 }
